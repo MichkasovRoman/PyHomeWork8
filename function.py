725ebd514755f94cc7d8906ec13fa474f08159f7
Michkasov Roman <mihjob@list.ru>
2023-01-29 23:32:52 +0300

    function

---
+++ function.py
@@ -0,0 +1,41 @@
+def IfNumber(line):
+    array = list(line)
+    for element in array:
+        if ord(element) < 48 or ord(element) > 57:
+            return False
+    return True
+
+def IfArithmeticSign(symbol):
+    if len(symbol) != 1:
+        return False
+    else:
+        if ord(symbol) != 42 and ord(symbol) != 43 and ord(symbol) != 45 and ord(symbol) != 47:
+            return False
+    return True
+
+def GetNameOfOperation(symbol):
+    if IfArithmeticSign(symbol):
+        if symbol == '+':
+            return 'сложения'
+        elif symbol == '-':
+            return 'вычитания'
+        elif symbol == '*':
+            return 'умножения'
+        elif symbol == '/':
+            return 'деления'
+    else:
+        return 'несуществующий символ'
+        
+
+def Operation(a, b, c):
+    if IfNumber(a) and IfNumber(c) and IfArithmeticSign(b):
+        if b == '+':
+            return(int(a) + int(c))
+        elif b == '-':
+            return(int(a) - int(c))
+        elif b == '*':
+            return(int(a) * int(c))
+        elif b == '/':  
+            return(int(a) / int(c))
+    else:
+        return 'Некорректный ввод данных. Повторите попытку'
