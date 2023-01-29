1f1b75d672b6d1f43582c5e508a2305a3419ad37
Michkasov Roman <mihjob@list.ru>
2023-01-30 01:13:25 +0300

    level2

--- level2.py
+++ level2.py
@@ -5,8 +5,8 @@ import function as fn
 
 expression = input('Введите числовое выражение из действий одной ступени: ')
 components = expression.split()
-result = fn.Operation(components[0], components[1], components[2])
-for i in range(3, len(components) - 1, 2):
+result = float(components[0])
+for i in range(1, len(components) - 1, 2):
     result = fn.Operation(str(result), components[i], components[i + 1])
 
 print(f'Ответ: {result}')
